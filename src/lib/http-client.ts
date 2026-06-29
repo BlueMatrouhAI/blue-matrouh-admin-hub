1262d67084ddba859261460bd4110144fd6e8744
Mori <122186077+Abn0Masr@users.noreply.github.com>
2026-06-30 00:48:27 +0300

    Remove token logging in HTTP request interceptor
    Remove console log for token in request interceptor

--- src/lib/http-client.ts
+++ src/lib/http-client.ts
@@ -8,7 +8,6 @@ const httpClient = axios.create({
 httpClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    console.log(token)
 
     if (token) {
       config.headers.Authorization = `${token}`;
